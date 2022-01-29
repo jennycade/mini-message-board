a9895e66f8d553bddcc160a45681f36e52500aad
Jenny Zonka <jennycade@gmail.com>
2022-01-29 10:55:15 -0800

    Move /new to index router. Handle form submission.

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var newRouter = require('./routes/new');
 
 var app = express();
 
@@ -21,8 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/new', newRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -20,4 +20,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mini message board', messages: messages });
 });
 
+/* new message form */
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'New message' });
+});
+
+/* form submission */
+router.post('/new', (req, res, next) => {
+  // should verify these somehow
+  const userName = req.body.user;
+  const text = req.body.text;
+
+  messages.push({
+    text: text,
+    user: userName,
+    added: new Date(),
+  });
+
+  res.redirect('/');
+});
+
 module.exports = router;
